ad41e09926fab6feced2eb5bf72639ac67c380de
charles <ch4rle2.fh@gmail.com>
2023-10-08 21:27:11 -0300

    Resolução do primeiro exercício

--- exercicio-01.ts
+++ exercicio-01.ts
@@ -1,14 +1,26 @@
+const inputPalavra = document.querySelector('#input') as HTMLInputElement
+const display = document.querySelector('.display') as HTMLParagraphElement
+
 function contarVogais(palavra: string): number {
-  const vogais = ['a', 'e', 'i', 'o', 'u']
-  let count = 0
+  const vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  let contador = 0
 
   for (let i = 0; i < palavra.length; i++) {
     if (vogais.includes(palavra[i])) {
-      count++
+      contador++
     }
   }
-
-  return count
+  return contador
 }
 
-console.log(contarVogais('banana'))
+// Primeira parte do exercício, exemplo de uso com uma palavra recebida via parâmetro da função.
+const palavra = 'Continuar'
+console.log(`A palavra ${palavra} possui ${contarVogais(palavra)} vogais`)
+
+// Segunda parte do exercício, exemplo de uso com uma palavra recebida via input do usuário.
+inputPalavra.addEventListener('keyup', (event: KeyboardEvent) => {
+  event.preventDefault()
+  display.textContent = `A palavra ${inputPalavra.value} possui ${contarVogais(
+    inputPalavra.value
+  )} vogais`
+})
