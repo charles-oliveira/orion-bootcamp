2645e9291f4851684a421fc669c9e0cab720db05
charles <ch4rle2.fh@gmail.com>
2023-10-09 15:57:23 -0300

    Feitas alterações relacionadas à PR #1

--- dist/exercicio-01.js
+++ dist/exercicio-01.js
@@ -2,20 +2,15 @@
 const inputPalavra = document.querySelector('#input');
 const display = document.querySelector('.display');
 function contarVogais(palavra) {
-    const vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
-    let contador = 0;
-    for (let i = 0; i < palavra.length; i++) {
-        if (vogais.includes(palavra[i])) {
-            contador++;
-        }
-    }
-    return contador;
+    const vogaisRegex = /[aeiouáàâãéêíóôõúü]/gi;
+    const vogaisEncontradas = palavra.match(vogaisRegex);
+    return vogaisEncontradas ? vogaisEncontradas.length : 0;
 }
-// Primeira parte do exercício, exemplo de uso com uma palavra recebida via parâmetro da função.
 const palavra = 'Continuar';
 console.log(`A palavra ${palavra} possui ${contarVogais(palavra)} vogais`);
-// Segunda parte do exercício, exemplo de uso com uma palavra recebida via input do usuário.
-inputPalavra.addEventListener('keyup', (event) => {
-    event.preventDefault();
-    display.textContent = `A palavra ${inputPalavra.value} possui ${contarVogais(inputPalavra.value)} vogais`;
-});
+if (inputPalavra && display) {
+    inputPalavra.addEventListener('keyup', (event) => {
+        event.preventDefault();
+        display.textContent = `A palavra ${inputPalavra.value} possui ${contarVogais(inputPalavra.value)} vogais`;
+    });
+}
--- src/exercicio-01.ts
+++ src/exercicio-01.ts
@@ -1,26 +1,23 @@
-const inputPalavra = document.querySelector('#input') as HTMLInputElement
-const display = document.querySelector('.display') as HTMLParagraphElement
+const inputPalavra: HTMLInputElement | null = document.querySelector('#input')
+const display: HTMLDivElement | null = document.querySelector('.display')
 
 function contarVogais(palavra: string): number {
-  const vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-  let contador = 0
+  const vogaisRegex = /[aeiouáàâãéêíóôõúü]/gi // g = global, i = case insensitive
+  const vogaisEncontradas = palavra.match(vogaisRegex) // retorna um array com as vogais encontradas
 
-  for (let i = 0; i < palavra.length; i++) {
-    if (vogais.includes(palavra[i])) {
-      contador++
-    }
-  }
-  return contador
+  return vogaisEncontradas ? vogaisEncontradas.length : 0 // se não encontrar nenhuma vogal, retorna 0
 }
 
 // Primeira parte do exercício, exemplo de uso com uma palavra recebida via parâmetro da função.
-const palavra = 'Continuar'
+const palavra: string = 'Continuar'
 console.log(`A palavra ${palavra} possui ${contarVogais(palavra)} vogais`)
 
 // Segunda parte do exercício, exemplo de uso com uma palavra recebida via input do usuário.
-inputPalavra.addEventListener('keyup', (event: KeyboardEvent) => {
-  event.preventDefault()
-  display.textContent = `A palavra ${inputPalavra.value} possui ${contarVogais(
-    inputPalavra.value
-  )} vogais`
-})
+if (inputPalavra && display) {
+  inputPalavra.addEventListener('keyup', (event: KeyboardEvent) => {
+    event.preventDefault()
+    display.textContent = `A palavra ${
+      inputPalavra.value
+    } possui ${contarVogais(inputPalavra.value)} vogais`
+  })
+}
