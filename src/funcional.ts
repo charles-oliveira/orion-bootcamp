fedb29c929c396b34b0071abb79abd544e95aa5b
charles <ch4rle2.fh@gmail.com>
2023-10-12 00:55:24 -0300

    Tipando todas as funções do paradigma funcional

--- src/funcional.ts
+++ src/funcional.ts
@@ -2,19 +2,19 @@
 import { lista } from './personalidades'
 
 // a) Retorna a bio a partir de um id passado PR-FUN
-const getBioByIdFuncional = (id: number) => {
+const getBioByIdFuncional = (id: number): string => {
   const item = lista.find(obj => obj['id'] === id)
   return item ? item['bio'] : 'ID não encontrado'
 }
 
 //  b) Retorna o name a partir de um id passado PR-FUN
-const getNameByIdFuncional = (id: number) => {
+const getNameByIdFuncional = (id: number): string => {
   const item = lista.find(obj => obj['id'] === id)
   return item ? item['name'] : 'ID não encontrado'
 }
 
 // c) Apaga um item da lista a partir de um id passado PR-FUN
-const deleteItemByIdFuncional = (id: number) => {
+const deleteItemByIdFuncional = (id: number): void => {
   const index = lista.findIndex(obj => obj['id'] === id) // Encontra o index do item
   // Se o index existir
   if (index !== -1) {
@@ -23,7 +23,11 @@ const deleteItemByIdFuncional = (id: number) => {
 }
 
 // d) Altera a bio ou o name a partir de um id passado PR-FUN
-const updateItemByIdFuncional = (id: number, name?: string, bio?: string) => {
+const updateItemByIdFuncional = (
+  id: number,
+  name?: string,
+  bio?: string
+): void => {
   const item = lista.find(obj => obj['id'] === id)
   if (item) {
     // Se o name for passado
